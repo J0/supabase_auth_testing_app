ffc361e213a4839d343d4ad017de10e5233f8392
joel@joellee.org <joel@joellee.org>
2023-08-01 16:57:29 +0200

    fix: update run generate link

--- app/generate_link/page.tsx
+++ app/generate_link/page.tsx
@@ -2,18 +2,11 @@ import { supabase } from "@/components/init";
 import { createClient } from "@supabase/supabase-js";
 
 export default function GenerateLink() {
-  async function generateSignUpLink() {
-    const { data, error } = await supabase.auth.admin.generateLink({
-      type: "magiclink",
-      email: "jacobsmith@gmail.com",
-      createUser: false,
-    });
-    console.log(data);
-  }
+
   return (
     <>
       <div> Generate link page </div>
-      <button onClick={generateSignUpLink}>Generate Sign Up Link</button>
+      <button onClick={()=>console.log("test")}>Generate Sign Up Link</button>
     </>
   );
 }
