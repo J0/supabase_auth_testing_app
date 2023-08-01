032db15fd71ee362ab639bec8927e302a64d3b57
joel@joellee.org <joel@joellee.org>
2023-08-01 17:00:38 +0200

    fix: remove cruft

--- app/generate_link/page.tsx
+++ app/generate_link/page.tsx
@@ -2,11 +2,10 @@ import { supabase } from "@/components/init";
 import { createClient } from "@supabase/supabase-js";
 
 export default function GenerateLink() {
-
   return (
     <>
       <div> Generate link page </div>
-      <button onClick={()=>console.log("test")}>Generate Sign Up Link</button>
+      <button onClick={() => console.log("test")}>Generate Sign Up Link</button>
     </>
   );
 }
